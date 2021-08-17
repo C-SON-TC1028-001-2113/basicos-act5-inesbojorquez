0da95baa316c42c58294f970ba930a16ffcaf0a2
inesbojorquez <a01252378@itesm.mx>
2021-08-17 19:59:23 +0000

    15Escalera

--- assignments/15Escalera/src/exercise.py
+++ assignments/15Escalera/src/exercise.py
@@ -1,6 +1,12 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-
+     h = int(input("Cual es la altura de la casa? "))
+     g = int(input("Cual es el angulo? "))
+     s = math.sin(g)
+     largo = h/s
+     print ("El largo de la escalera es:  "+str(math.ceil(largo)))
+   
+   
 if __name__ == '__main__':
     main()
